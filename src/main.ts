d8c53fa8cd7917854b5c91913397d558d32b9041
ilcann <ilcan21@itu.edu.tr>
2025-09-11 13:07:22 +0300

    feat: add validation pipes and update dependencies for class-transformer and class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const allowedOrigins = [
     `http://${process.env.FASTAPI_HOST}:${process.env.FASTAPI_PORT}`,
