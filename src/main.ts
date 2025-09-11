8f6a5e175958078d0e46e9d5acca99b58c120bf9
ilcann <ilcan21@itu.edu.tr>
2025-09-11 13:05:40 +0300

    feat: enhance API documentation setup and enable CORS with dynamic origins

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const allowedOrigins = [
+    `http://${process.env.FASTAPI_HOST}:${process.env.FASTAPI_PORT}`,
+    `http://${process.env.VITE_WEB_HOST}:${process.env.VITE_WEB_PORT}`,
+  ];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Secur Ai API Documentation')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addServer(`http://${process.env.NESTJS_HOST}:${process.env.NESTJS_PORT}`)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.NESTJS_PORT ?? 3002);
 }
 void bootstrap();
